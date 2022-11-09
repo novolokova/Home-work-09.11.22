fceadfc31bf27e782a951749130937341ad6be7e
Margarita <m.novolokova@gmail.com>
2022-11-09 15:10:30 +0200

    Add second method, task 1

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-
-// 1) написати функцію, яка повертає слово 'odd' або 'even', в залежності від прийнятого аргументу(число).
+// 1) написати функцію, яка повертає слово 'odd' або 'even', 
+// в залежності від прийнятого аргументу(число).
 // Значення для перевірки функції вводить користувач.
 
 let userInput = prompt("Enter your number: ");
 
 /**
  * 
+ * @param {string} value
+ * @returns {string}
+ */
+function checkUserInputNumber_1(value) {
+  if (value === "" || isNaN(value) || value === null || value === " ") {
+   
+    return "Odd";
+  }
+  return "Event";
+}
+
+let chek_1 = checkUserInputNumber_1(userInput);
+console.log(chek_1);
+
+/**
+ * 
  * @param {string} value 
  * @returns {string}
  */
-function checkUserInputNumber(value) {
-  if (value === "" || isNaN(value) || value === null) {
-    return 'Odd';
+function checkUserInputNumber_2(value) {
+      if (Number(value)){
+    return "Event";
   }
-  return 'Event';
+  return "Odd";
 }
-let chec = checkUserInputNumber(userInput);
-console.log(chec);
+
+let chek_2 = checkUserInputNumber_2(userInput);
+console.log(chek_2);
 
 
 
